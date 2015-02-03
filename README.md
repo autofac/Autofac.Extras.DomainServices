c7884dda7fb50b95e1618ebd6092f993348b4a87
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2015-02-03 22:47:29 +1000

    Added solution file and build configuration.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Autofac.Extras.DomainServices
 Autofac Domain Service Factory for RIA Services
+
+[![Build status](https://ci.appveyor.com/api/projects/status/7cy42ia922we7v3w?svg=true)](https://ci.appveyor.com/project/Autofac/autofac-extras-domainservices)
--- src/Autofac.Extras.DomainServices/Properties/AssemblyInfo.cs
+++ src/Autofac.Extras.DomainServices/Properties/AssemblyInfo.cs
@@ -5,3 +5,15 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Autofac.Extras.DomainServices")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
+
+[assembly: AssemblyCompany("Autofac Project - http://autofac.org")]
+[assembly: AssemblyProduct("Autofac")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCopyright("Copyright © 2014 Autofac Contributors")]
+[assembly: AssemblyDescription("Autofac Domain Service Factory for RIA Services")]
